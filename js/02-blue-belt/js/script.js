638859e8039ff7552730f0dd2b57f82692b6932d
Vladyslav <turbo.lukoshko@gmail.com>
2019-04-23 13:38:09 +0300

    fixed calc

--- js/02-blue-belt/js/script.js
+++ js/02-blue-belt/js/script.js
@@ -1,22 +1,5 @@
 /* Синий пояс Number: flats */
 
-var apartmentNum = +prompt('Введите номер квартиры', '1');
-var floorNum = 9; // Number of floors in the house
-var apartmentOnFloor = 4; // Number of apartments per floor
-var apartmentOnEntrance = floorNum * apartmentOnFloor; // Number of apartments in entrance
-var numberEntrace = Math.ceil(apartmentNum / apartmentOnEntrance); // Number entrance
-// Formula for finding apartment on the floor
-var numberFloor = Math.ceil((apartmentOnEntrance + apartmentNum - numberEntrace * apartmentOnEntrance) / apartmentOnFloor);
-// check on NaN
-if(isNaN(apartmentNum) || (apartmentNum == 0)) {
-  alert('Введите корректный номер квартиры');
-} else {
-  alert('Ваша квартира находится на ' + numberFloor + ' этаже, ' + numberEntrace + '-го подъезда');
-}
-
-
-/* method 2 */
-
 var apartment = +prompt('Введите номер квартиры', '1');
 var floors = 9; // Count floors in house
 var apartmentsOnFloor = 4; // Count apartments on floor
@@ -27,6 +10,14 @@ var floor = Math.ceil((apartment/apartmentsOnFloor) % 9); // Determinating floor
 // check on NaN
 if(isNaN(apartment) || (apartment == 0)){
   alert('Вы ввели некорректные данные. Повторите попытку');
-}else {
+}else if(floor == 0) {
+  /* 
+    Error in calc. 
+    Remainder from 36, 72, 108 and etc - will be equal 0.
+    Therefore, changed the formula for calc floor.
+  */
+  floor = Math.ceil((apartment/apartmentsOnFloor) / entrance);
+  alert('Подьезд №' + entrance + ' квартира на ' + floor + ' этаже');
+}else{
   alert('Подьезд №' + entrance + ' квартира на ' + floor + ' этаже');
 }
