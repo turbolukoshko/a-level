dc64b9072e0858dc5e7ec6aa382498aa5ed38ab8
Vladyslav <turbo.lukoshko@gmail.com>
2019-04-23 13:15:51 +0300

    added 2 method

--- js/02-blue-belt/js/script.js
+++ js/02-blue-belt/js/script.js
@@ -8,8 +8,25 @@ var numberEntrace = Math.ceil(apartmentNum / apartmentOnEntrance); // Number ent
 // Formula for finding apartment on the floor
 var numberFloor = Math.ceil((apartmentOnEntrance + apartmentNum - numberEntrace * apartmentOnEntrance) / apartmentOnFloor);
 // check on NaN
-if(isNaN(apartmentNum)) {
+if(isNaN(apartmentNum) || (apartmentNum == 0)) {
   alert('Введите корректный номер квартиры');
 } else {
   alert('Ваша квартира находится на ' + numberFloor + ' этаже, ' + numberEntrace + '-го подъезда');
+}
+
+
+/* method 2 */
+
+var apartment = +prompt('Введите номер квартиры', '1');
+var floors = 9; // Count floors in house
+var apartmentsOnFloor = 4; // Count apartments on floor
+
+var apartmentsOnEntrance = floors * apartmentsOnFloor; // Count apartments in entrance
+var entrance = Math.ceil(apartment / apartmentsOnEntrance); // Determinating entrance
+var floor = Math.ceil((apartment/apartmentsOnFloor) % 9); // Determinating floor in entrance
+// check on NaN
+if(isNaN(apartment) || (apartment == 0)){
+  alert('Вы ввели некорректные данные. Повторите попытку');
+}else {
+  alert('Подьезд №' + entrance + ' квартира на ' + floor + ' этаже');
 }
