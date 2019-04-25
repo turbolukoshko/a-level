7717098c2c1ff92caa0fa8eaeade371ce75dfac8
Vladyslav <turbo.lukoshko@gmail.com>
2019-04-25 15:07:38 +0300

    fix if ;

--- js/03-blue-belt/js/script.js
+++ js/03-blue-belt/js/script.js
@@ -8,7 +8,8 @@ var ratios = {
 var operation,
     resultRate,
     course,
-    amount;
+    amount,
+    checkAmountValid;
 
 var user = prompt('В какую валюту хотите обменять? USD или EUR', 'usd').toLowerCase();
 if(user == 'usd'){
@@ -17,12 +18,12 @@ if(user == 'usd'){
   amount = +prompt('Сумма для обмена', '');
   resultRate = amount / course;
   alert('Сумма обмена: ' + resultRate.toFixed(2) + '$');
-} if(user == 'eur'){
+}else if(user == 'eur'){
   operation = confirm('Покупка или продажа?');
   course = (operation) ? ratios['eur'] : ratios['sellEur'];
   amount = +prompt('Сумма для обмена?', '');
   resultRate = amount / course;
   alert('Сумма обмена: ' + resultRate.toFixed(2) + '€');
-}if((user == '') || (user != 'usd') || (user != 'eur')) {
+}else {
   alert('Введите корректную валюту для обмена');
 }
