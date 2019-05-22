ed9c209ab912a3e4abd77fe7595b90752288fe45
Vladyslav <turbo.lukoshko@gmail.com>
2019-05-22 19:14:14 +0300

    fixed function naming

--- js/08/js/script.js
+++ js/08/js/script.js
@@ -1,64 +1,63 @@
 /* Замыкания */
 /* makeProfileTimer */
-var makeProfileTimer = function(){
-  var start = performance.now();
+function makeProfileTimer(){
+  let start = performance.now();
   function calc(){
     let end = performance.now();
-    let result = end - start;
-    return result;
+    let diff = end - start;
+    return diff; 
   }
   return calc;
-};
+}
 
-var doSomething = function(){
-  alert('function doSomething has been started!');
-};
+function doSomething(){
+  alert('function doSomething has been started');
+}
 
 var timer = makeProfileTimer();
 doSomething();
 alert(timer());
 
 /* makeSaver */
-function makeSaver(arg){
-  var statament = true;
-  var randomNumber;
-  function comparisonValue(){
-    if(statament){
-      randomNumber = arg();
+function makeSaver(funcRandom){
+  let statement = true;
+  let randomNumber;
+  function comparisonNumber(){
+    if(statement){
+      randomNumber = funcRandom();
     }
-    statament = false;
+    statement = false;
     return randomNumber;
   }
-  return comparisonValue;
+  return comparisonNumber;
 }
 
 var saver = makeSaver(Math.random);
 var value1 = saver();
 var value2 = saver();
-
-console.log(value1 === value2);
+console.log(value1 == value2);
 
 var saver2 = makeSaver(() => console.log('saved function called') || [null, undefined, false, '', 0, Math.random()][Math.ceil(Math.random()*6)]);
 var value3 = saver2();
 var value4 = saver2();
-console.log(value3 == value4);
+console.log(value3 === value4);
 
 /* Final Countdown */
-function countDown(arg){
-  delay = 0;
-  for(var i = arg; i >= 0; i--){
-    (function(i){
+function countdown(num){
+  let delay = 0;
+  for(let i = num; i >= 0; i--){
+    (function(){
       setTimeout(function(){
-        if(i == 0){
+        if(i == 0 ){
           i = "поехали!";
         }
         console.log(i);
       }, delay += 1000);
-    })(i);
+    })();
   }
 }
 
-countDown(5);
+countdown(5);
 
 /* myBind */
 
