a89ac0105110ae71c8ec01fc8cdb91ae19f13035
Vladyslav <turbo.lukoshko@gmail.com>
2019-05-30 23:23:05 +0300

    stage 2

---
+++ js/11-chat/js/script.js
@@ -0,0 +1,46 @@
+function jsonPost(url, data)
+{
+    return new Promise((resolve, reject) => {
+        var x = new XMLHttpRequest();   
+        x.onerror = () => reject(new Error('jsonPost failed'))
+        //x.setRequestHeader('Content-Type', 'application/json');
+        x.open("POST", url, true);
+        x.send(JSON.stringify(data))
+
+        x.onreadystatechange = () => {
+            if (x.readyState == XMLHttpRequest.DONE && x.status == 200){
+                resolve(JSON.parse(x.responseText))
+            }
+            else if (x.status != 200){
+                reject(new Error('status is not 200'))
+            }
+        }
+    })
+}
+
+btn.addEventListener('click', () => {
+  jsonPost("http://students.a-level.com.ua:10012", {func: 'addMessage', nick: nick.value, message: message.value})
+});
+
+function getMessages(){
+  jsonPost("http://students.a-level.com.ua:10012", {func: "getMessages", messageId: 0})
+  .then(res => {
+    for(let i = 0; i < res.data.length; i++){
+      let container = document.createElement('div');
+      container.className = 'container';
+      let containerNick = document.createElement('h3');
+      containerNick.className = 'containerNick';
+      let containerMessage = document.createElement('p');
+      containerMessage.className = 'containerMessage';
+      containerNick.innerHTML = res.data[i].nick;
+      containerMessage.innerHTML = res.data[i].message;
+      container.appendChild(containerNick);
+      container.appendChild(containerMessage);
+      chatHistory.appendChild(container);
+      chatHistory.insertBefore(container, chatHistory.firstChild);
+      console.log(res.data[i]);
+    }
+  });
+}
+
+getMessages();
