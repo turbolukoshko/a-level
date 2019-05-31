c65c056fa421fb5ddc25584ad5987154275e37e3
Vladyslav <turbo.lukoshko@gmail.com>
2019-05-31 17:17:19 +0300

    stage 5

--- js/11-chat/js/script.js
+++ js/11-chat/js/script.js
@@ -17,33 +17,49 @@ function jsonPost(url, data)
         }
     })
 }
+var messageId = 0;
 
 btn.addEventListener('click', () => {
-  jsonPost("http://students.a-level.com.ua:10012", {func: 'addMessage', nick: nick.value, message: message.value})
+  // sendMessage(nick.value, message.value);
+  sendAndCheck();
 });
 
-(function(){
-  setInterval(function(){
-    var messageId = 0;
-      jsonPost("http://students.a-level.com.ua:10012", {func: "getMessages", messageId})
-        .then(res => {
-          for(var mess of res.data){
-            let container = document.createElement('div');
-            container.className = 'container';
-            let containerNick = document.createElement('h3');
-            containerNick.className = 'containerNick';
-            let containerMessage = document.createElement('p');
-            containerMessage.className = 'containerMessage';
-            containerNick.innerHTML = mess.nick;
-            containerMessage.innerHTML = mess.message;
-            container.appendChild(containerNick);
-            container.appendChild(containerMessage);
-            chatHistory.appendChild(container);
-            chatHistory.insertBefore(container, chatHistory.firstChild);
-          }
-          messageId = res.nextMessageId;
-          console.log(messageId);
-        });
-    }, 3500
-  );
-})();
+async function sendMessage(nick, message){
+    await jsonPost("http://students.a-level.com.ua:10012", {func: 'addMessage', nick: nick, message: message});
+}
+
+async function getMessages(){
+  let res = await jsonPost("http://students.a-level.com.ua:10012", {func: "getMessages", messageId})
+  for(var mess of res.data){
+    let container = document.createElement('div');
+    container.className = 'container';
+    let containerNick = document.createElement('h3');
+    containerNick.className = 'containerNick';
+    let containerMessage = document.createElement('p');
+    containerMessage.className = 'containerMessage';
+    containerNick.innerHTML = mess.nick;
+    containerMessage.innerHTML = mess.message;
+    container.appendChild(containerNick);
+    container.appendChild(containerMessage);
+    chatHistory.appendChild(container);
+    chatHistory.insertBefore(container, chatHistory.firstChild);
+    console.log(mess);
+  }
+  messageId = res.nextMessageId;
+  console.log(res.data.length);
+}
+
+async function sendAndCheck(){
+  await sendMessage(nick.value, message.value);
+  let hist = await getMessages();
+}
+
+async function checkLoop(){
+  const delay = ms => new Promise(ok => setTimeout(() => ok(ms), ms))
+  while(true){
+    await getMessages();
+    await delay(2000);
+  }
+}
+
+checkLoop();
