ec9f0d0b18297c5cf9f67fc75fcb5b449e9f8793
Vladyslav <turbo.lukoshko@gmail.com>
2019-06-03 18:54:39 +0300

    add recursion

--- js/08/js/script.js
+++ js/08/js/script.js
@@ -1,74 +1,75 @@
-/* Замыкания */
-/* makeProfileTimer */
-function makeProfileTimer(){
-  let start = performance.now();
-  function calc(){
-    let end = performance.now();
-    let diff = end - start;
-    return diff; 
-  }
-  return calc;
-}
+// /* Замыкания */
+// /* makeProfileTimer */
+// function makeProfileTimer(){
+//   let start = performance.now();
+//   function calc(){
+//     let end = performance.now();
+//     let diff = end - start;
+//     return diff; 
+//   }
+//   return calc;
+// }
 
-function doSomething(){
-  alert('function doSomething has been started');
-}
+// function doSomething(){
+//   alert('function doSomething has been started');
+// }
 
-var timer = makeProfileTimer();
-doSomething();
-alert(timer());
+// var timer = makeProfileTimer();
+// doSomething();
+// alert(timer());
 
-/* makeSaver */
-function makeSaver(funcRandom){
-  let statement = true;
-  let randomNumber;
-  function comparisonNumber(){
-    if(statement){
-      randomNumber = funcRandom();
-    }
-    statement = false;
-    return randomNumber;
-  }
-  return comparisonNumber;
-}
+// /* makeSaver */
+// function makeSaver(funcRandom){
+//   let statement = true;
+//   let randomNumber;
+//   function comparisonNumber(){
+//     if(statement){
+//       randomNumber = funcRandom();
+//     }
+//     statement = false;
+//     return randomNumber;
+//   }
+//   return comparisonNumber;
+// }
 
-var saver = makeSaver(Math.random);
-var value1 = saver();
-var value2 = saver();
-console.log(value1 == value2);
+// var saver = makeSaver(Math.random);
+// var value1 = saver();
+// var value2 = saver();
+// console.log(value1 == value2);
 
-var saver2 = makeSaver(() => console.log('saved function called') || [null, undefined, false, '', 0, Math.random()][Math.ceil(Math.random()*6)]);
-var value3 = saver2();
-var value4 = saver2();
-console.log(value3 === value4);
+// var saver2 = makeSaver(() => console.log('saved function called') || [null, undefined, false, '', 0, Math.random()][Math.ceil(Math.random()*6)]);
+// var value3 = saver2();
+// var value4 = saver2();
+// console.log(value3 === value4);
 
-/* Final Countdown */
-function countdown(num){
-  let delay = 0;
-  for(let i = num; i >= 0; i--){
-    (function(){
-      setTimeout(function(){
-        if(i == 0 ){
-          i = "поехали!";
-        }
-        console.log(i);
-      }, delay += 1000);
-    })();
-  }
-}
+// /* Final Countdown */
+// function countdown(num){
+//   let delay = 0;
+//   for(let i = num; i >= 0; i--){
+//     (function(){
+//       setTimeout(function(){
+//         if(i == 0 ){
+//           i = "поехали!";
+//         }
+//         console.log(i);
+//       }, delay += 1000);
+//     })();
+//   }
+// }
 
-countdown(5);
+// countdown(5);
 
 /* myBind */
 
 /* Рекурсия */
 /* Sum */
-// a1 - first num
-// n - coefficient
-// function sum(a1, n, step){
-//   let an = a1 + (n - 1) * step; // find last 
-//   let sum = (a1 + an) / 2 * n; 
-//   return sum;
-// }
 
-// alert(sum(1,2,1));
+function recursion(n){
+  if(n == 1){
+    return 1;
+  }else{
+    return n + recursion(n-1);
+  }
+}
+
+console.log(recursion(3));
